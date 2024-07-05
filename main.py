726f3c7496ae8cb900ac65f4356d6254e377e557
Nikita <kniki098@gmail.com>
2024-07-05 15:27:15 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app=FastAPI()
 
+#request Get method url: "/"
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
