d21363e67247696c0d1fee267aea863c1a16e72a
Nikita <kniki098@gmail.com>
2024-07-05 18:11:47 +0530

    Get and post method

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app=FastAPI()
 
 #request Get method url: "/"
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
@@ -10,3 +12,8 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title {payload['title']} content: {payload['content']}"}
