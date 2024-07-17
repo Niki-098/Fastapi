06129c7fb06b8b1b73dfb215b88f5f5c95ba0efa
Nikita <kniki098@gmail.com>
2024-07-17 19:12:21 +0530

    Created routers folder

--- app/main.py
+++ app/main.py
@@ -8,8 +8,10 @@ from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
 from sqlalchemy.sql.functions import mode
-from . import models ,schemas
+from . import models ,schemas, utils
 from .database import engine,get_db
+
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -132,10 +134,26 @@ def update_post(id: int,updated_post: schemas.PostCreate,db:Session = Depends(ge
 
 @app.post("/users",status_code=status.HTTP_201_CREATED,response_model = schemas.UserOut)
 def create_user(user:schemas.UserCreate,db:Session = Depends(get_db)):
+
+    #hash the password - user.password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
     return new_user
+
+
+@app.get('/users/{id}',response_model=schemas.UserOut)
+def get_user(id: int, db:Session = Depends(get_db)):
+    user =  db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND
+                 ,detail=f"User with id: {id} does not exist")
+    
+    return user
 
