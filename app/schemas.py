56449eec2551fa6205daeec031484e67287e5e39
Nikita <kniki098@gmail.com>
2024-07-18 23:42:16 +0530

    Created JWT token,Oauth2 password request form,verify user logged in

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,7 @@
 
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
+from typing import Optional
 
 
 
@@ -36,3 +37,10 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
