16bd77b1a5f8ea6373e5637be6033db37ae7fd17
Nikita <kniki098@gmail.com>
2024-07-24 16:14:47 +0530

    Cleanup main.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost/fastapi'
 
@@ -17,3 +19,17 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost',database='fastapi',user='postgres'
+            ,password='1234',cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successfull!")
+        break
+    except Exception as error:
+        print("Connectiong to database failed")
+        print("Error: ",error)
+        #time.sleep(2)
