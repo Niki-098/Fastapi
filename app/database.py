98fabd09a61613d96f7a055caace828b0b21e90e
Nikita <kniki098@gmail.com>
2024-08-03 01:52:11 +0530

    albemy installation

--- app/database.py
+++ app/database.py
@@ -3,8 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import psycopg2
 from psycopg2.extras import RealDictCursor
+import time
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost/fastapi'
+#SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost/fastapi'
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:
+{settings.database_password}@{settings.database_hostname}:
+{settings.database_port}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
  
@@ -22,14 +28,14 @@ def get_db():
 
 
 
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost',database='fastapi',user='postgres'
-            ,password='1234',cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was successfull!")
-        break
-    except Exception as error:
-        print("Connectiong to database failed")
-        print("Error: ",error)
-        #time.sleep(2)
+# while True:
+#     try:
+#         conn = psycopg2.connect(host='localhost',database='fastapi',user='postgres'
+#             ,password='1234',cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was successfull!")
+#         break
+#     except Exception as error:
+#         print("Connectiong to database failed")
+#         print("Error: ",error)
+#         #time.sleep(2)
